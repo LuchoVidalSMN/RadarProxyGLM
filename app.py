13038d34054ae1d4f5b41c2def2b953b3003e2dc
LuchoVidalSMN <lucianovidal78@gmail.com>
2026-09-04 12:54:58 -0300

    Se modifico la funcion para analisis multivariado

--- app.py
+++ app.py
@@ -234,7 +234,7 @@ def plot_parallel_coordinates(metrics_df, highlight_poly_id=None):
         "MCS": "#e76f51",
     }
 
-    fig, ax = plt.subplots(figsize=(10, 2))
+    fig, ax = plt.subplots(figsize=(10, 4))
 
     # 1. Dibujar líneas de cada polígono
     for _, row in df_norm.iterrows():
